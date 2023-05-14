e250e54157406bd497b39ba90f36db9d356db214
LetsgoShaun <hzsunhongen@163.com>
2023-05-14 16:31:08 +0100

    first change

---
+++ src/data/generated/bib.js
@@ -0,0 +1,111 @@
+﻿define({ entries : {
+    "bargshady2019joint": {
+        "author": "Bargshady, Ghazal and Soar, Jeffrey and Zhou, Xujuan and Deo, Ravinesh C and Whittaker, Frank and Wang, Hua",
+        "booktitle": "2019 IEEE 4th International Conference on Computer and Communication Systems (ICCCS)",
+        "keywords": "type:Pain,Databases,Feature extraction,Neural networks,Face,Face recognition,Shoulder",
+        "organization": "IEEE",
+        "pages": "52--56",
+        "title": "A joint deep neural network model for pain recognition from face",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "bargshady2020enhanced": {
+        "author": "Bargshady, Ghazal and Zhou, Xujuan and Deo, Ravinesh C and Soar, Jeffrey and Whittaker, Frank and Wang, Hua",
+        "journal": "Expert Systems with Applications",
+        "keywords": "type:Facial expressionPain detectionDeep neural networksExpert systems in healthcareMachine learning",
+        "pages": "113305",
+        "publisher": "Elsevier",
+        "title": "Enhanced deep learning algorithm development to detect pain intensity from facial expression images",
+        "type": "article",
+        "volume": "149",
+        "year": "2020"
+    },
+    "bargshady2020ensemble": {
+        "author": "Bargshady, Ghazal and Zhou, Xujuan and Deo, Ravinesh C and Soar, Jeffrey and Whittaker, Frank and Wang, Hua",
+        "journal": "Artificial Intelligence in Medicine",
+        "keywords": "type:Ensemble neural network,Pain detection,Facial expression, Deep learning",
+        "pages": "101954",
+        "publisher": "Elsevier",
+        "title": "Ensemble neural network approach detecting pain intensity from facial expressions",
+        "type": "article",
+        "volume": "109",
+        "year": "2020"
+    },
+    "bargshady2020modeling": {
+        "author": "Bargshady, Ghazal and Zhou, Xujuan and Deo, Ravinesh C and Soar, Jeffrey and Whittaker, Frank and Wang, Hua",
+        "journal": "Applied Soft Computing",
+        "keywords": "type:Temporal convolutional network,Facial expression,Pain detection,HSV color space,Video analysis",
+        "pages": "106805",
+        "publisher": "Elsevier",
+        "title": "The modeling of human facial pain intensity based on Temporal Convolutional Networks trained with video frames in HSV color space",
+        "type": "article",
+        "volume": "97",
+        "year": "2020"
+    },
+    "chen2018automated": {
+        "author": "Chen, Zhanli and Ansari, Rashid and Wilkie, Diana",
+        "journal": "arXiv preprint arXiv:1811.07988",
+        "keywords": "type:Action Unit, Automated Pain Detection, Facial Action Coding System, Automated Facial Expression Recognition,Deep Learning",
+        "title": "Automated pain detection from facial expressions using facs: A review",
+        "type": "article",
+        "year": "2018"
+    },
+    "gong2022deep": {
+        "author": "Gong, Wenjuan and An, Zhihong and Elfiky, Noha M",
+        "journal": "Neural Computing and Applications",
+        "keywords": "type:Microexpression recognition,Deep learning,Survey",
+        "number": "12",
+        "pages": "9537--9560",
+        "publisher": "Springer",
+        "title": "Deep learning-based microexpression recognition: a survey",
+        "type": "article",
+        "volume": "34",
+        "year": "2022"
+    },
+    "rodriguez2017deep": {
+        "author": "Rodriguez, Pau and Cucurull, Guillem and Gonz{\\`a}lez, Jordi and Gonfaus, Josep M and Nasrollahi, Kamal and Moeslund, Thomas B and Roca, F Xavier",
+        "journal": "IEEE transactions on cybernetics",
+        "keywords": "type:Pain , Feature extraction , Hidden Markov models , Face , Estimation , Databases , Face recognition",
+        "number": "5",
+        "pages": "3314--3324",
+        "publisher": "IEEE",
+        "title": "Deep pain: Exploiting long short-term memory networks for facial expression classification",
+        "type": "article",
+        "volume": "52",
+        "year": "2017"
+    },
+    "tavakolian2018deep": {
+        "author": "Tavakolian, Mohammad and Hadid, Abdenour",
+        "booktitle": "2018 24th International Conference on Pattern Recognition (ICPR)",
+        "keywords": "type:Pain , Kernel , Three-dimensional displays , Convolution , Feature extraction , Estimation , Spatiotemporal phenomena",
+        "organization": "IEEE",
+        "pages": "350--354",
+        "title": "Deep spatiotemporal representation of the face for automatic pain intensity estimation",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "wang2018pain": {
+        "author": "Wang, Jinwei and Sun, Huazhi",
+        "journal": "IEICE TRANSACTIONS on Information and Systems",
+        "keywords": "type:pain intensity estimation,  3D convolutional network,  histogram of oriented gradients,  feature fusion",
+        "number": "6",
+        "pages": "1572--1580",
+        "publisher": "The Institute of Electronics, Information and Communication Engineers",
+        "title": "Pain intensity estimation using deep spatiotemporal and handcrafted features",
+        "type": "article",
+        "volume": "101",
+        "year": "2018"
+    },
+    "xin2020pain": {
+        "author": "Xin, Xuwu and Lin, Xiaoyan and Yang, Shengfu and Zheng, Xin",
+        "journal": "Plos one",
+        "keywords": "type:Pain, face, imaging techniques,Neural network",
+        "number": "8",
+        "pages": "e0232412",
+        "publisher": "Public Library of Science San Francisco, CA USA",
+        "title": "Pain intensity estimation based on a spatial transformation and attention CNN",
+        "type": "article",
+        "volume": "15",
+        "year": "2020"
+    }
+}});
